40b3f45d84196cde9722bb9d78e899978a2fcd03
ronaldoapf <ronaldo.alves.1997@gmail.com>
2024-08-30 16:58:05 -0300

    create delete answer use case and tests

--- test/repositories/in-memory-answers-repository.ts
+++ test/repositories/in-memory-answers-repository.ts
@@ -7,4 +7,19 @@ export class InMemoryAnswersRepository implements AnswersRepository {
   async create(answer: Answer): Promise<void> {
     this.items.push(answer)
   }
+
+  async delete(answer: Answer): Promise<void> {
+    const itemIndex = this.items.findIndex((item) => item.id === answer.id)
+    this.items.splice(itemIndex, 1)
+  }
+
+  async findById(id: string): Promise<Answer | null> {
+    const answer = this.items.find((item) => item.id.toString() === id)
+
+    if (!answer) {
+      return null
+    }
+
+    return answer
+  }
 }

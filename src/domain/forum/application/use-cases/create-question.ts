74d701d6e88b9b9e4c670ed7897ec47f871abab9
ronaldoapf <ronaldo.alves.1997@gmail.com>
2024-08-30 16:17:04 -0300

    refactor tests

---
+++ src/domain/forum/application/use-cases/create-question.ts
@@ -0,0 +1,33 @@
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+import { QuestionsRepository } from '../repositories/questions-repository'
+import { Question } from '../../enterprise/entities/question'
+
+interface CreateQuestionUseCaseRequest {
+  authorId: string
+  title: string
+  content: string
+}
+
+interface CreateQuestionUseCaseResponse {
+  question: Question
+}
+
+export class CreateQuestionUseCase {
+  constructor(private questionsRepository: QuestionsRepository) {}
+
+  async execute({
+    authorId,
+    content,
+    title,
+  }: CreateQuestionUseCaseRequest): Promise<CreateQuestionUseCaseResponse> {
+    const question = Question.create({
+      title,
+      content,
+      authorId: new UniqueEntityId(authorId),
+    })
+
+    await this.questionsRepository.create(question)
+
+    return { question }
+  }
+}
